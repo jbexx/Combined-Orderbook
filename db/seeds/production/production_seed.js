c26954bdad351b711b020580eef72fb5133c781d
Jonathan Beckman <jbecks.media@gmail.com>
2017-10-26 15:14:31 -0600

    add production seed data and file

---
+++ db/seeds/production/production_seed.js
@@ -0,0 +1,40 @@
+const bittrexBook = require('../../../bittrex-production-book.json');
+const poloniexBook = require('../../../poloniex-production-book.json');
+
+const createPoloniexBook = (knex, bid, ask) => {
+  return knex('poloniex_book').insert({
+    bid_volume: bid[1],
+    bid: bid[0],
+    ask: ask[0],
+    ask_volume: ask[1]
+  });
+};
+
+const createBittrexBook = (knex, bid, ask) => {
+  return knex('bittrex_book').insert({
+    bid_volume: bid.Quantity,
+    bid: bid.Rate,
+    ask: ask.Rate,
+    ask_volume: ask.Quantity
+  });
+};
+
+exports.seed = (knex, Promise) => {
+  return knex('bittrex_book').del()
+    .then( () => knex('poloniex_book').del() )
+    .then( () => {
+      const poloniexOrders = [];
+      const bittrexOrders = [];
+
+      for (let i = 0; i < poloniexBook.bids.length; i++) {
+        poloniexOrders.push( createPoloniexBook(knex, poloniexBook.bids[i], poloniexBook.asks[i]) );
+      }
+
+      for (let j = 0; j < bittrexBook.result.buy.length; j++) {
+        bittrexOrders.push( createBittrexBook(knex, bittrexBook.result.buy[j], bittrexBook.result.sell[j]) );
+      }
+
+      return Promise.all([ ...poloniexOrders, ...bittrexOrders ]);
+    })
+    .catch(error => console.error('Error seeding data', error));    
+};
--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev',
+      directory: './db/seeds/production',
     },
     useNullAsDefault: true
   },
